9051b3694be92583ea660bfd2f44de81cd0719e8
Gantt-sucessor <javarealfan@gmail.com>
2026-04-29 10:58:00 -0400

    Criando entidade do banco de dados

--- src/main/java/com/fazendo/dengue/Entity/DengueEntity.java
+++ src/main/java/com/fazendo/dengue/Entity/DengueEntity.java
@@ -1,4 +1,24 @@
 package com.fazendo.dengue.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_dados_dengue")
+@Data
 public class DengueEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long cidadeId;
+    private String cidadeNome;
+    private Integer casos;
+    private Integer obitos;
+    private LocalDate data;
+
+
 }
